81637c78bb82ae5676220bd3bb3856f11bb6487e
Po6aT <zveruga007@inbox.ru>
2020-10-07 09:48:58 +0300

    find minimum

--- homework1/src/com/bercut/oct/Min.java
+++ homework1/src/com/bercut/oct/Min.java
@@ -2,6 +2,18 @@ package com.bercut.oct;
 
 public class Min {
     public static void main(String[] args) {
-        System.out.println("Gav");
+        int min = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        if (b < min){
+            min =b;
+        }
+        if (c < min){
+            min =c;
+        }
+        System.out.println(min + " is the smallest");
+
+
     }
 }
